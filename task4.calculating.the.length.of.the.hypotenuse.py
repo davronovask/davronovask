4606c9f16c4facbdc199af441cb2fa85e5b0accb
User <abdulkhaevamuslima07@gmail.com>
2024-05-08 19:58:53 +0600

    first commit

---
+++ task4.calculating.the.length.of.the.hypotenuse.py
@@ -0,0 +1,5 @@
+katet1 = int(input("введиту первую сторону катета"))
+katet2 = int(input("введиту вторую сторону катета"))
+summa = (katet1 ** 2) + (katet2 ** 2)
+hypotenuse = summa ** (1 / 2)
+print(hypotenuse)
